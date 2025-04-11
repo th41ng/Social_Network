badbc31920469831141a17018def7100fbe2134f
capchencanh <danhvip83@gmail.com>
2025-04-12 00:24:11 +0700

    Initial commit: Thêm cấu trúc dự án ban đầu

---
+++ SpringSocialApp/src/main/java/com/socialapp/configs/WebAppContextConfigs.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.socialapp.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author DELL G15
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {
+    "com.socialapp.controllers"
+   
+})
+public class WebAppContextConfigs implements WebMvcConfigurer{
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+  
+   
+}
---
+++ SpringSocialApp/src/main/java/com/socialapp/service/impl/SurveyResponseServiceImpl.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.socialapp.service.impl;
+
+import com.socialapp.service.SurveyResponseService;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author DELL G15
+ */
+@Service
+public class SurveyResponseServiceImpl implements SurveyResponseService{
+    
+}
